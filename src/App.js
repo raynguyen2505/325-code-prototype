97dd6734a5bdfd0360b8dd19eebcd3672690f86f
raynguyen2505 <90473047+raynguyen2505@users.noreply.github.com>
2021-11-20 16:53:45 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        series: [{
+            name: 'Capacity',
+            data: ["23", "31", "40", "101", "40", "36", "32", "23", "14", "8", "5", "2"]
+          }],
+          options: {
+            chart: {
+              height: 350,
+              type: 'bar',
+            },
+            plotOptions: {
+              bar: {
+                borderRadius: 10,
+                dataLabels: {
+                  position: 'top', // top, center, bottom
+                },
+              }
+            },
+            dataLabels: {
+              enabled: true,
+              formatter: function (val) {
+                return val;
+              },
+              offsetY: -20,
+              style: {
+                fontSize: '12px',
+                colors: ["#304758"]
+              }
+            },
+            
+            xaxis: {
+              categories: ["1pm", "2pm", "3pm", "NOW", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12am"],
+              position: 'top',
+              axisBorder: {
+                show: false
+              },
+              axisTicks: {
+                show: false
+              },
+              crosshairs: {
+                fill: {
+                  type: 'gradient',
+                  gradient: {
+                    colorFrom: '#D8E3F0',
+                    colorTo: '#BED1E6',
+                    stops: [0, 100],
+                    opacityFrom: 0.4,
+                    opacityTo: 0.5,
+                  }
+                }
+              },
+              tooltip: {
+                enabled: true,
+              }
+            },
+            yaxis: {
+              axisBorder: {
+                show: false
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: {
+                show: false,
+                formatter: function (val) {
+                  return val;
+                }
+              }
+            
+            },
+          },
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="bar"
+              width="1200"
+              height="600"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
