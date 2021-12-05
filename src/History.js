9f45aa1fc283c29a323d3749ce59b4a3bca92cfc
Hung Nguyen <hnguyenmanh@umass.edu>
2021-12-05 17:46:28 -0500

    changes for homepage

---
+++ src/History.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+import React from 'react';
+
+const History = () => {
+	return(
+		<div className="History">
+			<h1>yoyoyo its history</h1>
+		</div>
+	)
+}
+
+export default History;
