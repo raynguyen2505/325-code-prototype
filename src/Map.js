42a306a93a5cf7752e71cbbb39ca663255967405
Jiangyifei Zhu <47931455+ZJendex@users.noreply.github.com>
2021-12-05 14:32:53 -0500

    map page rebuilt with dynamical heat-map

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,35 @@
 // JavaScript source code
-import React from 'react';
-import myMap from "./recMap.PNG"
+import React, {useRef, useEffect} from 'react';
+import myMap from "./reccentermap_page-0001.jpg"
 import './Map.css'
 
-function Map() {
-	return(
+const Map = props => {
+
+	const canvasRef = useRef(null);
+	const tar = 40;
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		const context = canvas.getContext('2d');
+		//Our first draw
+		context.globalAlpha = 0.4;
+		// activity room 118
+		context.fillStyle = `rgb(${tar*9}, ${256 - tar*9}, 0)`;
+		context.fillRect(790, 240, 100, 100)
+		// activity room 121
+		context.fillStyle = `rgb(${15*9}, ${256 - 15*9}, 0)`;
+		context.fillRect(910, 240, 80, 90)
+
+	}, [])
+
+	return (
 		<div className="Map">
-			<img src={myMap}></img>
+			<canvas ref={canvasRef} width={1275} height={1650} {...props}/>
 		</div>
 	)
 }
 
 export default Map;
+
+// <div className="Map">
+// 	<img src={myMap}></img>
+// </div>
